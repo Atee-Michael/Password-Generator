fd3c860f3215e4e9f4edbacc533eaf0e71cbc014
Michael Atee <ateemichael@yahoo.com>
2023-02-22 22:12:16 +0000

    modified JS

--- script.js
+++ script.js
@@ -1,31 +1,62 @@
-// Get references to the elements
-const generateBtn = document.getElementById("generate");
-const passwordEl = document.getElementById("password");
+// Array of character types
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const specialChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
 
-// Sets for password criteria
-const lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
-const upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numericChars = "0123456789";
-const specialChars = "$@%&*()_+-=[]{}|<>?,.";
+// Function to generate password
+function generatePassword() {
+let password = "";
+let passwordLength = parseInt(prompt("How many characters would you like your password to be? (8-128)"));
 
-// Function to prompt user for password criteria and generate the password
+// Validate password length
+while(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+passwordLength = parseInt(prompt("Invalid input. Please enter a number between 8 and 128."));
+}
 
+// Character type prompts
+let includeLowerCase = confirm("Click ok if you want to include lowercase characters?");
+let includeUpperCase = confirm("Click ok if you want to include uppercase characters?");
+let includeNumbers = confirm("Click ok if you want to include numbers?");
+let includeSpecialChars = confirm("Click ok if you want to include special characters?");
 
-// Prompt user for length of password
+// Validate at least one character type selected
+while(!includeLowerCase && !includeUpperCase && !includeNumbers && !includeSpecialChars) {
+alert("Please select at least one character type.");
+includeLowerCase = confirm("Click ok if you want to include lowercase characters?");
+includeUpperCase = confirm("Click ok if you want to include uppercase characters?");
+includeNumbers = confirm("Click ok if you want to include numbers?");
+includeSpecialChars = confirm("Click ok if you want to include special characters?");
+}
 
+// Loop to generate password
+let possibleChars = "";
+if(includeLowerCase) {
+possibleChars += lowerCase;
+}
+if(includeUpperCase) {
+possibleChars += upperCase;
+}
+if(includeNumbers) {
+possibleChars += numbers;
+}
+if(includeSpecialChars) {
+possibleChars += specialChars;
+}
 
+for(let i = 0; i < passwordLength; i++) {
+password += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+}
 
+return password;
+}
 
-// Prompt user for character types
+// Function to write password to the page
+function writePassword() {
+let passwordText = document.querySelector("#password");
+let password = generatePassword();
+passwordText.value = password;
+}
 
-
-
-
-// Generate the password
-
-
-
-// Display the password
-
-
-// Add event listener to generate button
+// Event listener to generate password on button click
+document.querySelector("#generate").addEventListener("click", writePassword);
