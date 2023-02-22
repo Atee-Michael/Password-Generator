3679e4fb2f33081ea594199fc67f44ccd954a1e3
Michael Atee <ateemichael@yahoo.com>
2023-02-22 21:54:06 +0000

    added pseudo code for project

--- script.js
+++ script.js
@@ -8,82 +8,24 @@ const upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numericChars = "0123456789";
 const specialChars = "$@%&*()_+-=[]{}|<>?,.";
 
+// Function to prompt user for password criteria and generate the password
 
 
-/*
-function promptForPassword() {
-  // Prompt the user for a password
-  var password = prompt("Enter your password:");
-
-  // Check that the password is at least 8 characters long
-  if (password.length < 8) {
-    alert("Error: Password must be at least 8 characters long.");
-    return;
-  }
-
-  // Check that the password contains at least one uppercase letter
-  if (!/[A-Z]/.test(password)) {
-    alert("Error: Password must contain at least one uppercase letter.");
-    return;
-  }
-
-  // Check that the password contains at least one lowercase letter
-  if (!/[a-z]/.test(password)) {
-    alert("Error: Password must contain at least one lowercase letter.");
-    return;
-  }
-
-  // Check that the password contains at least one number
-  if (!/[0-9]/.test(password)) {
-    alert("Error: Password must contain at least one number.");
-    return;
-  }
-
-  // If all checks pass, print a success message
-  alert("Success: Password is valid.");
-}
-
-*/
+// Prompt user for length of password
 
 
 
-// Function for getting a random element from an array
-function getRandom(possibleCharacters) {
 
-  let practicePassword = ""
-  for (let i = 0; i < lengthOfPassword; i++) {
-    let randomCharacter =[Math.floor(Math.random() * possibleCharacters.length)];
-    practicePassword = practicePassword + possibleCharacters[randomCharacter];
-  }
-  return finalPassword1;
-};
+// Prompt user for character types
 
 
-// Function to generate password with user input
-function generatePassword() {
 
-  let finalPassword = "";
-  for (let i = 0; i < 100; i++) {
-    
-    finalPassword += "jo"
-    
-  }
-  return finalPassword;  
 
-}
+// Generate the password
 
-// Get references to the #generate element
-let generateBtn = document.querySelector('#generate');
 
-// Write password to the #password input
-function writePassword() {
-  let password = generatePassword();
-  let passwordText = document.querySelector('#password');
 
-  passwordText.value = password;
-}
-
+// Display the password
 
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
